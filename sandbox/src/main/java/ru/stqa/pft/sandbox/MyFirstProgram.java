caacc7c89ad01e27b656d27f335f789149fb9664
Leontyeva <orchid345617@gmail.com>
2018-09-19 18:27:26 +0300

    дополнила

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {System.out.println("Hello, world!");}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
 
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(1.0 / 2.0);
+
+        System.out.println("2" + "2"); // конкатенация
+        System.out.println(2 + "2"); // число всегда преобразовывается в строку, затем просиходит конкатенация, выражение = 22
+
+    }
 }
