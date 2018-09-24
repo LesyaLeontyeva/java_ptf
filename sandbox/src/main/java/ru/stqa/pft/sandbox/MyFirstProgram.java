b686009555962639e67722c0ef843ecf66541a7e
Leontyeva <orchid345617@gmail.com>
2018-09-24 13:34:25 +0300

    Добавлены несколько новых функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,21 +1,47 @@
 package ru.stqa.pft.sandbox;
 
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("User");
+        hello("Olesya");
 
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
+        double l = 5;
+        System.out.println("Площать квадрата со стороной " + l + " = " + area(l));
 
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-        System.out.println(1.0 / 2.0);
+        double a = 4;
+        double b = 8;
+        System.out.println(("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b)));
 
-        System.out.println("2" + "2"); // конкатенация
-        System.out.println(2 + "2"); // число всегда преобразовывается в строку, затем просиходит конкатенация, выражение = 22
+//        System.out.println(2 + 2);
+//        System.out.println(2 * 2);
+//        System.out.println(2 / 2);
+//        System.out.println(2 - 2);
+//
+//        System.out.println(1 / 2);
+//        System.out.println(1.0 / 2);
+//        System.out.println(1 / 2.0);
+//        System.out.println(1.0 / 2.0);
+//
+//        System.out.println("2" + "2"); // конкатенация
+//        System.out.println(2 + "2"); // число всегда преобразовывается в строку, затем просиходит конкатенация, выражение = 22
+//        double w = 8.0;
+//        double s = w * w;
+//        System.out.println("Площадь квадрата со стороной " + w + " = " + s);
 
+
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area(double len){
+        return len*len;
+    }
+
+    public static double area(double a, double b){
+        return a*b;
     }
 }
