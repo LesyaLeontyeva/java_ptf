9a0bfee768902943a473b293c0242f308fd5a771
Leontyeva <orchid345617@gmail.com>
2018-10-12 11:23:51 +0300

    добавлено много чего

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,11 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
+
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
 
 
 //    protected final ApplicationManager app = new ApplicationManager();
@@ -13,6 +18,11 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private int browser;
+
+    public ApplicationManager(int browser) {
+        this.browser = browser;
+    }
 
 //    public static boolean isAlertPresent(FirefoxDriver wd) {
 //        try {
@@ -24,7 +34,15 @@ public class ApplicationManager {
 //    }
 
     public void init() {
-        wd = new FirefoxDriver();
+//        Integer browser = BrowserType.MOZILLA;
+        if (browser == BrowserType.MOZILLA) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.DEFAULT) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.INTERNET_EXPLORER){
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
