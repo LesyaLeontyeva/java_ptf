a727d234841d438db5fe6b5ee78495cda862edc8
Leontyeva <orchid345617@gmail.com>
2018-11-19 18:59:25 +0300

    123

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,18 +1,59 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public GroupData(int id, String name, String header, String footer) {
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+        this.id = id;
+    }
 
     public GroupData(String name, String header, String footer) {
         this.name = name;
         this.header = header;
         this.footer = footer;
+        this.id = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 
     public String getHeader() {
@@ -22,4 +63,6 @@ public class GroupData {
     public String getFooter() {
         return footer;
     }
+
+
 }
